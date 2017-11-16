5887f219873a0bc298c0a689a2ed444029d5c66f
Visen11 <visenvenum@gmail.com>
2017-11-17 00:42:34 +0100

    Dodatak2

--- Projekat12/Server/ServerClass.cs
+++ Projekat12/Server/ServerClass.cs
@@ -9,6 +9,8 @@ namespace Server
 {
     public class ServerClass : IServer
     {
+        private static readonly Object lockObject = new Object();
+
         public bool AzurirajPotrosnju(DataObj potrosac)
         {
             //Audit.AzuriranjePotrosnje();
@@ -17,14 +19,32 @@ namespace Server
 
         public bool DodajEntitet(DataObj noviPotrosac)
         {
+            foreach (var potrosac in Program.lokalnaBaza)
+                if (potrosac.Id == noviPotrosac.Id)
+                    return false;
+
+            lock(lockObject)
+            {
+                Program.lokalnaBaza.Add(noviPotrosac);
+            }
             //Audit.DodavanjeEntiteta();
             return true;
         }
 
         public bool ObrisiEntitet(string id)
         {
-            //Audit.BrisanjeEntiteta();
-            return true;
+            foreach (var potrosac in Program.lokalnaBaza)
+                if (potrosac.Id == id)
+                {
+                    lock(lockObject)
+                    {
+                        potrosac.Obrisan = true;
+                    }
+                    //Audit.BrisanjeEntiteta();
+                    return true;
+                }
+            
+            return false;
         }
 
         public void PrikazInformacija()
