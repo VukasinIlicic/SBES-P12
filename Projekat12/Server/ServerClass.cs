c0dd2459ff1461d4291a0e2711d226c1c0ae1d47
tomislav.kostic <tomislavkostic95@hotmail.com>
2017-11-19 19:58:41 +0100

    Refaktorisana ConvertMonthToIndex metoda

--- Projekat12/Server/ServerClass.cs
+++ Projekat12/Server/ServerClass.cs
@@ -1,6 +1,7 @@
 ﻿using Common;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -121,35 +122,7 @@ namespace Server
 
         private int ConvertMonthToIndex(string month)
         {
-            switch(month)
-            {
-                case "January":
-                    return 0;
-                case "February":
-                    return 1;
-                case "March":
-                    return 2;
-                case "April":
-                    return 3;
-                case "May":
-                    return 4;
-                case "June":
-                    return 5;
-                case "July":
-                    return 6;
-                case "August":
-                    return 7;
-                case "September":
-                    return 8;
-                case "October":
-                    return 9;
-                case "November":
-                    return 10;
-                case "December":
-                    return 11;
-                default:
-                    return 0;
-            }
-        }
+			return DateTime.ParseExact(month, "MMMM", CultureInfo.CurrentCulture).Month - 1;
+		}
     }
 }
